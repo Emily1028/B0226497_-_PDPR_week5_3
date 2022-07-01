c0036e30246705cfb990c605c4210fa00f083f11
Emily1028 <81636593+Emily1028@users.noreply.github.com>
2022-07-01 20:58:36 +0800

    加入專案檔案。

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace B0226497_段昱如_PDPR_week5_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("B0226497_段昱如_PDPR_week5_3");
+            Console.WriteLine("  VCD     單價  數量    總價");
+            Console.WriteLine("============================");
+            Console.WriteLine("哈利波特  650   1000  " + 650 * 1000);
+            Console.WriteLine("巴黎魅影  789   1500  " + 789 * 1500);
+            Console.ReadLine();
+        }
+    }
+}
